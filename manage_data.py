203615d59819937fc5568499d217e762d322f6e8
dnjooiopa <dnjooiopa@gmail.com>
2020-09-11 16:01:28 +0700

    write train, val, test to files

--- manage_data.py
+++ manage_data.py
@@ -21,3 +21,16 @@ random.shuffle(temp_image_list)
 train_list=temp_image_list[:train_len]
 val_list=temp_image_list[train_len:train_len+val_len]
 test_list=temp_image_list[train_len+val_len:]
+
+def write_to_file(file_path, data):
+    f = open(file_path, "w")
+    content = ''
+    for i in data:
+        content += i+'\n'
+    f.write(content)
+    f.close()
+
+write_to_file('data/train_crop_weed.txt', train_list)
+write_to_file('data/val_crop_weed.txt', val_list)
+write_to_file('data/test_crop_weed.txt', test_list)
+
