b3377046cfcb9bde69de94087da210b71e393348
dnjooiopa <dnjooiopa@gmail.com>
2020-09-11 15:34:18 +0700

    calculate train, validation and test length

--- manage_data.py
+++ manage_data.py
@@ -1,10 +1,15 @@
 import os
+import random
 
 # get all image files
-path_to_data = (os.getcwd()+"\\data\\image").replace('\\', '\\\\')
-image_list = [i for i in os.listdir(path_to_data) if '.jpeg' in i]
+path_to_data = os.getcwd().replace('\\', '/')+"/data/image"
+image_list = [(path_to_data+"/"+i)
+              for i in os.listdir(path_to_data) if ('.txt' not in i)]
 
-
+# split data
 percent_train = 0.7
 percent_val = 0.2
-percent_test = 1-(percent_train+percent_val)
+
+train_len = int(len(image_list) * percent_train)
+val_len = int(len(image_list) * percent_val)
+test_len = len(image_list) - (train_len+val_len)
